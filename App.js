f8a895fb28e2a6ac27ee18839aa59b7da25524a0
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-05-10 11:02:45 +0530

    Import createNativeStackNavigator

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Text } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
   return (
     <TailwindProvider>
       <NavigationContainer>
-        
+
       </NavigationContainer>
     </TailwindProvider>
   );
