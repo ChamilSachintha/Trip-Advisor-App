5f1a384646a37f526e94fa9cecb50ffa92221f37
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-05-10 11:01:43 +0530

    Import NavigationContainer

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Text } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <TailwindProvider>
-      <SafeAreaView className="flex-1 items-center justify-center">
-        <Text className="text-red-600">Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
+      <NavigationContainer>
+        
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
