fd5c83b8a9b1bbefea19a340da48c3a543af68a5
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-05-11 13:08:55 +0530

    Remove header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,17 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+
+  const navigation = useNavigation();
+
+  useLayoutEffect (() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, []);
+
   return (
     <View>
       <Text className="text-red-400">HomeScreen</Text>
