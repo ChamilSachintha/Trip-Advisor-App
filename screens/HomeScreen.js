8b43d48be0d59e46cd0c8898505ced8fb488c1a3
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-05-12 10:30:53 +0530

    Add 3 text sections

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,10 +17,17 @@ const HomeScreen = () => {
       {/* 1st Section */}
       <View className="flex-row px-6 mt-12 items-center space-x-2">
         <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
-          <Text className="text-[#40AB87] text-3xl font-semibold">Go</Text>
+          <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
         </View>
 
-        <Text>Travel</Text>
+        <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+      </View>
+
+      {/* 2nd Section */}
+      <View className="px-6 mt-8">
+        <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
+        <Text className="text-[#00BCC9] text-[38px] font-bold">Good Moments</Text>
+        <Text className="text-[#3C6072] text-base mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Text>
       </View>
     </SafeAreaView>
   )
