c88f09643db7c60825213132a870f152d7bd249d
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-06-08 22:23:20 +0530

    Touchable button added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { HeroImg } from '../assets';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const HomeScreen = () => {
 
@@ -16,7 +17,7 @@ const HomeScreen = () => {
   return (
     <SafeAreaView className="bg-white flex-1 relative">
       {/* 1st Section */}
-      <View className="flex-row px-6 mt-12 items-center space-x-2">
+      <View className="flex-row px-6 mt-5 items-center space-x-2">
         <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
           <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
         </View>
@@ -25,7 +26,7 @@ const HomeScreen = () => {
       </View>
 
       {/* 2nd Section */}
-      <View className="px-6 mt-8">
+      <View className="px-6 mt-6">
         <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
         <Text className="text-[#00BCC9] text-[38px] font-bold">Good Moments</Text>
         <Text className="text-[#3C6072] text-base mt-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Text>
@@ -37,7 +38,16 @@ const HomeScreen = () => {
 
       {/* Image container */}
       <View className="flex-1 relative items-center justify-center">
-        <Image source={HeroImg} className="w-full h-full object-cover mt-8"/>
+        <Image source={HeroImg} 
+        className="w-full h-full object-cover mt-8"/>
+
+      <View className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[#00BCC9] rounded-full items-center justify-center">
+      <TouchableOpacity>
+        <View className="w-20 h-20 items-center justify-center rounded-full bg-[#00BCC9]">
+          <Text className="text-gray-50 text-[36px] font-semibold">Go</Text>
+        </View>
+        </TouchableOpacity>
+      </View>
       </View>
     </SafeAreaView>
   )
