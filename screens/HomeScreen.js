9b8c90a915cc5c65a0d80dc99d26bc608a781aea
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2023-06-15 21:26:14 +0530

    Pulse animation & onPress event added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,9 @@ import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { HeroImg } from '../assets';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
 import * as Animatable from 'react-native-animatable';
+import { TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
 
@@ -46,10 +47,18 @@ const HomeScreen = () => {
         className="w-full h-full object-cover mt-8"/>
 
       <View className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[#00BCC9] rounded-full items-center justify-center">
-      <TouchableOpacity>
-        <View className="w-20 h-20 items-center justify-center rounded-full bg-[#00BCC9]">
+      <TouchableOpacity
+        onPress={() => {
+        // Handle onPress event
+        }}
+      >
+        <Animatable.View 
+          animation={"pulse"} 
+          easing={"ease-in-out"}
+          iterationCount={"infinite"}
+          className="w-20 h-20 items-center justify-center rounded-full bg-[#00BCC9]">
           <Text className="text-gray-50 text-[36px] font-semibold">Go</Text>
-        </View>
+        </Animatable.View>
         </TouchableOpacity>
       </View>
       </View>
